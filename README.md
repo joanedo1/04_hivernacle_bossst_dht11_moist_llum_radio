0c0095fc73e10552487e60c67ee5c11659dce4d4
joan vicente edo beltran <136701188+joanedo1@users.noreply.github.com>
2025-11-17 14:55:50 +0100

    Fitxers inicials per al projecte MakeCode

---
+++ README.md
@@ -0,0 +1,24 @@
+
+> Obre aquesta pàgina a [https://joanedo1.github.io/04_hivernacle_bossst_dht11_moist_llum_radio/](https://joanedo1.github.io/04_hivernacle_bossst_dht11_moist_llum_radio/)
+
+## Utilitza-ho com a extensió
+
+Aquest dipòsit es pot afegir com una **extensió** a MakeCode.
+
+* obre [https://makecode.microbit.org/](https://makecode.microbit.org/)
+* fes clic a **Projecte nou**
+* fes clic a **Extensions** sota el menú de la roda dentada
+* cercar **https://github.com/joanedo1/04_hivernacle_bossst_dht11_moist_llum_radio** i importar
+
+## Edita aquest projecte
+
+Per editar aquest repositori a MakeCode.
+
+* obre [https://makecode.microbit.org/](https://makecode.microbit.org/)
+* fes clic a ** Importa** i després a ** Importa URL**
+* enganxa **https://github.com/joanedo1/04_hivernacle_bossst_dht11_moist_llum_radio** i clica importar
+
+#### Metadades (utilitzades per a la cerca, renderització)
+
+* for PXT/microbit
+<script src="https://makecode.com/gh-pages-embed.js"></script><script>makeCodeRender("{{ site.makecode.home_url }}", "{{ site.github.owner_name }}/{{ site.github.repository_name }}");</script>
---
+++ main.ts
@@ -0,0 +1,67 @@
+radio.onReceivedNumber(function (receivedNumber) {
+    DadaDemandada = receivedNumber
+    EnviaValors()
+})
+input.onButtonPressed(Button.A, function () {
+    mBit2()
+})
+function Sensors () {
+    basic.pause(1000)
+    dht11_dht22.queryData(
+    DHTtype.DHT11,
+    DigitalPin.P16,
+    true,
+    false,
+    true
+    )
+    HumDHT11 = dht11_dht22.readData(dataType.humidity)
+    TempDHT11 = dht11_dht22.readData(dataType.temperature)
+    HumSolP1 = pins.analogReadPin(AnalogReadWritePin.P1)
+    LlumP2 = pins.analogReadPin(AnalogReadWritePin.P2)
+}
+function Tot_a_0 () {
+    LlumP2 = 0
+    HumSolP1 = 0
+    HumDHT11 = 0
+    TempDHT11 = 0
+}
+function EnviaValors () {
+    if (DadaDemandada == 1) {
+        radio.sendValue("Temp:", TempDHT11)
+    } else if (DadaDemandada == 2) {
+        radio.sendValue("Hum:", HumDHT11)
+    } else if (DadaDemandada == 3) {
+        radio.sendValue("Lux:", LlumP2)
+    } else {
+        radio.sendValue("Reg:", HumSolP1)
+    }
+}
+function mBit2 () {
+    basic.showString("Temp ")
+    basic.showNumber(TempDHT11)
+    basic.showString("*C ")
+    basic.showString("Hum ")
+    basic.showNumber(HumDHT11)
+    basic.showString("% ")
+    basic.showString("Llum")
+    basic.showNumber(LlumP2)
+    basic.showString("Reg")
+    basic.showNumber(HumSolP1)
+    basic.pause(2000)
+    basic.showString("")
+}
+let LlumP2 = 0
+let HumSolP1 = 0
+let TempDHT11 = 0
+let HumDHT11 = 0
+let DadaDemandada = 0
+basic.pause(500)
+radio.setGroup(1)
+Tot_a_0()
+dht11_dht22.queryData(
+DHTtype.DHT11,
+DigitalPin.P0,
+true,
+false,
+true
+)
